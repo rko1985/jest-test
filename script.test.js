500099f31acc7e148386531da054532a5d11eecc
Russell Oyama <russelloyama@gmail.com>
2023-02-21 06:41:54 -1000

    writing our first test

--- script.test.js
+++ script.test.js
@@ -1,3 +1,18 @@
-it('this is a test', () => {
-    
+const googleSearch = require('./script');
+
+dbMock = [
+    'dog.com',
+    'cheesepuff.com',
+    'disney.com',
+    'dogpictures.com'
+];
+
+it('is a silly test', () => {
+    expect('hello').toBe('hello');
+    googleSearch('testtest', dbMock)
+})
+
+it('is seaching google', () => {
+    expect(googleSearch('testtest', dbMock)).toEqual([])
+    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
 })
