48226e37e5984daf135738a5e0c8041ddec29293
Russell Oyama <russelloyama@gmail.com>
2023-02-22 06:34:33 -1000

    writing more tests

--- script.test.js
+++ script.test.js
@@ -7,12 +7,24 @@ dbMock = [
     'dogpictures.com'
 ];
 
-it('is a silly test', () => {
-    expect('hello').toBe('hello');
-    googleSearch('testtest', dbMock)
+describe('googleSeach', () => {
+    it('is a silly test', () => {
+        expect('hello').toBe('hello');
+        googleSearch('testtest', dbMock)
+    })
+    
+    it('is seaching google', () => {
+        expect(googleSearch('testtest', dbMock)).toEqual([])
+        expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
+    })
+    
+    it('work with undefined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([]);
+    })
+    
+    it('does not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3);
+    })
 })
 
-it('is seaching google', () => {
-    expect(googleSearch('testtest', dbMock)).toEqual([])
-    expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com'])
-})
